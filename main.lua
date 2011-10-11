17326c5dbda51c7aedc245e1e83bc2fabf5c7dc6
Eric Gillet <fear_hardcore@yahoo.fr>
2011-10-11 23:39:17 +0200

    Ongoing Object-oriented transformation

--- main.lua
+++ main.lua
@@ -1,62 +1,82 @@
 --================ Class definitions
-Shot = {} --Shot class
-function Shot.create(width, height, mode, speed, damage, angle)
-    local sht = {}
-    sht.x = players[1].x
-    sht.y = players[1].y
-    sht.angle = ch_angle
-    sht.speed = speed
-    sht.damage = damage
-	sht.width = width
-	sht.height = height
-	sht.mode = mode
-	sht.image = love.graphics.newImage("bullet" .. mode .. ".png")
-    -- vector of bullet
-    sht.vx = math.cos(angle)
-    sht.vy = math.sin(angle)
-    return sht
+Shot = {
+x = 0,
+y = 0,
+angle = 0,
+speed = 100,
+damage = 100,
+width = 10,
+height = 3,
+mode = 1,
+image = love.graphics.newImage("bullet" .. 1 .. ".png"),
+-- vector of bullet
+vx = math.cos(0),
+vy = math.sin(0),
+}
+
+function Shot:create (o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    o.image=love.graphics.newImage("bullet" .. o.mode .. ".png")
+    o.vx = math.cos(o.angle)
+    o.vy = math.sin(o.angle)
+    return o
 end
 
-Bot = {} -- Bots table
-function Bot.create(x, y, speed, rew) -- no more speed argument ? (determined by rew ?)
-    local bt = {}
-    bt.x = x
-    bt.y = y
-    --bt.max_health = math.random(50,200) -- random health
-    --bt.max_health = math.random(rew/20 +10, rew/2) -- health determined by rew (+10 is for low rewarding bots)
-	bt.max_health = 100 --old fixed health or should the same class bots have same health ?
-    bt.health = bt.max_health
-	bt.color = bt.health -- not sure if this will not be screwed by random health
-	
-	bt.width = 20
-	bt.height = 20
-	bt.reward = rew --Gain obtained by player killing it
-	bt.angle = 0
-	bt.vx = 0
-	bt.vy = 0
-	
-	--bt.speed = speed
-	bt.speed = (rew/10) / bt.max_health -- that will make bots with lots of HP slow
-	function bt.hit(hitter)
-		bt.health = bt.health - hitter.damage
+Bot = {
+	x = 0,
+	y = 0,
+	max_health = 100,
+	health = 100,
+	alpha = 255,
+	width = 20,
+	height = 20,
+	reward = 100,
+	angle = 0,
+	vx = 0,
+	vy = 0,
+	speed = 0.1 -- that will make bots with lots of HP slow
+}
+
+function Bot:create (o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    o.health = o.max_health
+    o.alpha = o.health/o.max_health * 255
+    o.speed = (o.reward/10) / o.max_health -- that will make bots with lots of HP slow
+    return o
+end
+
+function Bot:hit(hitter)
+		self.health = self.health - hitter.damage
 	end
-    return bt
-end
 
-Player = {}
-function Player.create(x,y)
-	local pl = {}
-	pl.x = x
-	pl.y = y
-	pl.width = 10
-	pl.height = 10
-	pl.firemode = 0
-	pl.ch_angle = 0
-	pl.speed = 300
-	return pl
+Player = {
+	x = 0,
+	y = 0,
+	width = 20,
+	height = 20,
+	firemode = 0,
+	ch_angle = 0,
+	speed = 300,
+}
+
+function Player:create (o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
 end
 
 --================
+
+--Replace the i element by the last in table and shrink the table by one
+function remove_l(t, i) 
+	t[i]=t[#t]
+	table.remove(t)
+end
 
 function check_collision(a, b)
 if (a.x + a.width > b.x) and (a.x < b.x + b.width) and (a.y + a.height > b.y) and (a.y < b.y + b.height) then
@@ -81,9 +101,12 @@ function spawn_bot()
 			x2,y2,x2,y2 = win_width, win_height,x2+20,y2+20
 		end
 	end
-	sp = math.random(1,20)/10
-	--x1,x2,y1,y2 = math.min(x1,x2),math.max(x1,x2),math.min(y1,y2),math.max(y1,y2)
-	table.insert(bots,Bot.create(math.random(x1, x2), math.random(y1, y2), sp, sp*1000))
+	local sp = math.random(1,20)/10
+	table.insert(bots,Bot.create{ x=math.random(x1, x2), 
+								  y=math.random(y1, y2), 
+								  speed = sp,
+								  max_health=100,
+								  reward=sp*1000 })
 end
 
 function move_bots(b) -- Move bots around
@@ -92,7 +115,7 @@ function move_bots(b) -- Move bots around
 	for i,v in ipairs(b) do
 		if b[i].health <= 0 then
 			score = score + b[i].reward
-			table.remove(b,i)
+			remove_l(b,i) -- delete the last bot in table
 			break
 		end
 		old_x=b[i].x
@@ -105,6 +128,8 @@ function move_bots(b) -- Move bots around
 		if(check_collision(b[i], players[1])) then
 			Game = false
 		end
+		
+		--if player try to GTFO, stop it!
 		if (b[i].x < 0 or  b[i].x > win_width or b[i].y < 0 or b[i].y > win_height) then
 			b[i].x = old_x
 			b[i].y = old_y
@@ -117,12 +142,11 @@ function adv_shots(s, obj, dt) -- Move the shots further and check collisions
 		s[i].x = s[i].x + dt * s[i].speed * s[i].vx
 		s[i].y = s[i].y + dt * s[i].speed * s[i].vy
 		if ( s[i].x < 0 or s[i].x > win_width or s[i].y < 0 or s[i].y > win_height) then
-			table.remove(s, i)
+			remove_l(s, i)
 		else
 			--Check collision between shots and items
 			for j, y in ipairs(obj) do
 				if check_collision(s[i],obj[j]) then
-					boom = boom + 1
 					obj[j].hit(s[i])
 				end
 			end
@@ -144,7 +168,7 @@ function love.load()
 	
 	-- player definitions
 	players = {}
-	players[1] = Player.create(win_width/2, win_height/2)
+	players[1] = Player.create{x=win_width/2, y=win_height/2}
     
     score = 0
 	
@@ -161,7 +185,7 @@ function love.load()
     cooldown = 0.3 -- delay before new shot, in seconds
     last_shot = cooldown -- time since last shot, initialized to allow immediate shooting at start
     bots = {} --Table containing all the bots
-	table.insert(bots,Bot.create(5, 90, 1, 10)) --Create a first bot
+	--table.insert(bots,Bot.create(5, 90, 1, 10)) --Create a first bot
     shots = {} -- table of player shots
     boom = 0 -- Number of hits
 	bot_ctr = 0 -- Time since last spawn of bot
@@ -184,30 +208,38 @@ function love.update(dt)
     elseif love.keyboard.isDown("up") and players[1].y > 0 then
         players[1].y = players[1].y - (players[1].speed * dt)
     end
-    shot_type = "cl" -- ugly hack for 2 lines under
     if (love.mouse.isDown( "l" ) and last_shot >= cooldown) then
-        table.insert(shots, Shot.create(8, 3, 1, 400, 20, ch_angle))
+        table.insert(shots, Shot.create{ width=8, 
+										 height=3, 
+										 speed=400, 
+										 damage=20, 
+										 angle=players[1].ch_angle,
+										 vx=math.cos(players[1].ch_angle),
+										 vy=math.sin(players[1].ch_angle)
+										 })
         last_shot = 0
     else
         last_shot = last_shot + dt
     end
+    
     adv_shots(shots, bots, dt)
-	--if love.mouse.isDown( "r" ) then
 	move_bots(bots, dt)
-	--end
+	
     mouse_x = love.mouse.getX()
     mouse_y = love.mouse.getY()
-    ch_angle=-math.atan2((mouse_x-players[1].x),(mouse_y-players[1].y)) + math.pi/2
-    ch_x1=math.cos(ch_angle)*ch_iradius+players[1].x
-    ch_y1=math.sin(ch_angle)*ch_iradius+players[1].y
-    ch_x2=math.cos(ch_angle)*ch_oradius+players[1].x
-    ch_y2=math.sin(ch_angle)*ch_oradius+players[1].y
+    
+    --Calculate crosshair coords
+    players[1].ch_angle=-math.atan2((mouse_x-players[1].x),(mouse_y-players[1].y)) + math.pi/2
+    ch_x1=math.cos(players[1].ch_angle)*ch_iradius+players[1].x
+    ch_y1=math.sin(players[1].ch_angle)*ch_iradius+players[1].y
+    ch_x2=math.cos(players[1].ch_angle)*ch_oradius+players[1].x
+    ch_y2=math.sin(players[1].ch_angle)*ch_oradius+players[1].y
 end
 
 function love.draw()
     --love.graphics.print("Hello World", 400, 300)
     --love.graphics.point(x,y)
-    love.graphics.draw( ship, players[1].x , players[1].y, ch_angle, 2, 2, 8, 8 )
+    love.graphics.draw( ship, players[1].x , players[1].y, players[1].ch_angle, 2, 2, 8, 8 )
     love.graphics.line( ch_x1, ch_y1, ch_x2, ch_y2 )
     for i, v in ipairs(shots) do
 		love.graphics.draw (shots[i].image , shots[i].x, shots[i].y, shots[i].angle, 1, 1)
@@ -218,12 +250,15 @@ function love.draw()
 		love.graphics.circle ("line", bots[j].x, bots[j].y, 10, 3)
     end
 	love.graphics.setColor(255,255,255,255)
+	
+	
     --=== Debug
     --love.graphics.print (bots[j].x ,45, 120)
     --love.graphics.print(last_shot, 100, 200)
     --if (#shots >= 1) then
     --love.graphics.print(math.floor(shots[1].x) .. "*" .. math.floor(shots[1].y), 100, 100)
     --end
+    
      if (score) then
          love.graphics.print("Score : " .. score, 100, 300)
      end
@@ -234,5 +269,5 @@ function love.draw()
     -- end
     --love.graphics.print(shots[2].x .. shots[2].y, 100, 200)
     
-    --===
+    --=== End debug
 end
