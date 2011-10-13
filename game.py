5cf5cbbc0ba473765a6dc6e4b0f6dbd2360d23f3
Eric G. <fear_hardcore@yahoo.fr>
2011-10-13 18:34:05 +0300

    Unfinished

--- game.py
+++ game.py
@@ -61,16 +61,55 @@ def load():
 
 		screen.blit(background, (0, 0))
 		pygame.display.flip()
-
-def update():
 	
+def update(dt):
+bot_ctr = bot_ctr + dt
+if ( bot_ctr >= 0.2) then
+bot_ctr = 0
+spawn_bot()
+end
+    if (love.keyboard.isDown("right") and players[1].x < win_width) then
+        players[1].x = players[1].x + (players[1].speed * dt)
+    elseif (love.keyboard.isDown("left") and players[1].x > 0) then
+        players[1].x = players[1].x - (players[1].speed * dt)
+    end
+    
+    if (love.keyboard.isDown("down") and players[1].y < win_height ) then
+        players[1].y = players[1].y + (players[1].speed * dt)
+    elseif love.keyboard.isDown("up") and players[1].y > 0 then
+        players[1].y = players[1].y - (players[1].speed * dt)
+    end
+    if (love.mouse.isDown( "l" ) and last_shot >= cooldown) then
+        table.insert(shots, Shot.create{ width=8,
+height=3,
+speed=400,
+damage=20,
+angle=players[1].ch_angle,
+vx=math.cos(players[1].ch_angle),
+vy=math.sin(players[1].ch_angle)
+})
+        last_shot = 0
+    else
+        last_shot = last_shot + dt
+    end
+    
+    adv_shots(shots, bots, dt)
+move_bots(bots, dt)
 
+    mouse_x = love.mouse.getX()
+    mouse_y = love.mouse.getY()
+    
+    --Calculate crosshair coords
+    players[1].ch_angle=-math.atan2((mouse_x-players[1].x),(mouse_y-players[1].y)) + math.pi/2
+    ch_x1=math.cos(players[1].ch_angle)*ch_iradius+players[1].x
+    ch_y1=math.sin(players[1].ch_angle)*ch_iradius+players[1].y
+    ch_x2=math.cos(players[1].ch_angle)*ch_oradius+players[1].x
+    ch_y2=math.sin(players[1].ch_angle)*ch_oradius+players[1].y
 
-if __name__ == '__main__':
-	load()
-	while 1:
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				return
-		update()
+load()
+while 1:
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			return
+	update()
 		draw()
